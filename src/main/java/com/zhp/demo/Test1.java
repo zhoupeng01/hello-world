5dcfa264130267d9ec18157a3a208e29dbf16ac2
zhoupeng <927765306@qq.com>
2016-11-08 10:51:24 +0800

    git test

---
+++ src/main/java/com/zhp/demo/Test1.java
@@ -0,0 +1,12 @@
+package com.zhp.demo;
+
+public class Test1 {
+
+    private static String sayHello(){
+        return "hello zhp";
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(sayHello());
+    }
+}
